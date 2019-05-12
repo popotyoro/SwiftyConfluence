cb729ad67243ca1f2d2d9ba4b571724a55687d3a
Takumi Karibe <takumi.k.5610@gmail.com>
2019-05-12 22:58:18 +0900

    implement create

--- Sources/Model/Page.swift
+++ Sources/Model/Page.swift
@@ -8,11 +8,15 @@
 import Foundation
 
 public struct Page {
+    public let title: String
+    public let body: String
     public let url: URL
     public let spaceKey: String
     public let ancestorsKey: String?
     
-    public init(url: URL, spaceKey: String, ancestorsKey: String?) {
+    public init(title: String, body: String, url: URL, spaceKey: String, ancestorsKey: String?) {
+        self.title = title
+        self.body = body
         self.url = url
         self.spaceKey = spaceKey
         self.ancestorsKey = ancestorsKey?.isEmpty == true ? nil : ancestorsKey
--- Sources/SwiftyConfluence/SwiftyConfluence.swift
+++ Sources/SwiftyConfluence/SwiftyConfluence.swift
@@ -1,5 +1,5 @@
 //
-//  Page.swift
+//  SwiftyConfluence.swift
 //  SwiftyConfluence
 //
 //  Created by Takumi Karibe on 2019/05/11.
@@ -7,8 +7,49 @@
 
 import Foundation
 import SwiftyAtlassian
+import Confluence
 import Model
 
 public extension Atlassian.Confluence where Infrastructure == Server, Category == Content {
-    func create(page: Page) {}
+    func create(page: Page) {
+        let body: [String: Any] = {
+            var body: [String: Any] = [:]
+            body["type"] = "page"
+            body["title"] = page.title
+            body["space"] = ["key": page.spaceKey]
+            if let ancestorsKey = page.ancestorsKey {
+                body["ancestors"] = [["id": ancestorsKey]]
+            }
+            body["body"] = ["storage": ["value": page.body, "representation": "storage"]]
+            return body
+        }()
+
+        let semaphore = DispatchSemaphore(value: 0)
+        var request = URLRequest(url: page.url)
+
+        request.allHTTPHeaderFields = header
+        request.httpMethod = "POST"
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: [])
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            defer { semaphore.signal() }
+
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode else {
+                print("⛔️ not found status code")
+                return
+            }
+
+            if (200...203).contains(statusCode) {
+                print("✅ \(String(describing: response))")
+            } else {
+                print("🚫 statusCode: \(statusCode)")
+                if let error = error {
+                    print("🚨 error: \(error)")
+                }
+            }
+        }
+
+        task.resume()
+        semaphore.wait()
+    }
 }
