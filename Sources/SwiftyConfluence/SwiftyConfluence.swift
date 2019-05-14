07d70e8591dfaab12a0f8229f8971d8a4a598190
TakumiKaribe <takumi.k.5610@gmail.com>
2019-05-14 19:21:48 +0900

    fix structure

--- Sources/SwiftyConfluence/SwiftyConfluence.swift
+++ Sources/SwiftyConfluence/SwiftyConfluence.swift
@@ -11,59 +11,20 @@ import SwiftyAtlassian
 public protocol ConfluenceAPI {}
 
 public extension Atlassian.Confluence {
-    struct AccessMode: APICategory & ConfluenceAPI {
-        public let config: Config
-        public init(withAuth config: Config) { self.config = config }
-    }
-    struct Audit: APICategory & ConfluenceAPI {
-        public let config: Config
-        public init(withAuth config: Config) { self.config = config }
-    }
-    struct Content: APICategory & ConfluenceAPI {
-        public let config: Config
-        public init(withAuth config: Config) { self.config = config }
-    }
-    struct ContentBody: APICategory & ConfluenceAPI {
-        public let config: Config
-        public init(withAuth config: Config) { self.config = config }
-    }
-    struct Group: APICategory & ConfluenceAPI {
-        public let config: Config
-        public init(withAuth config: Config) { self.config = config }
-    }
-    struct LongTask: APICategory & ConfluenceAPI {
-        public let config: Config
-        public init(withAuth config: Config) { self.config = config }
-    }
-    struct Search: APICategory & ConfluenceAPI {
-        public let config: Config
-        public init(withAuth config: Config) { self.config = config }
-    }
-    struct Space: APICategory & ConfluenceAPI {
-        public let config: Config
-        public init(withAuth config: Config) { self.config = config }
-    }
-    struct User: APICategory & ConfluenceAPI {
-        public let config: Config
-        public init(withAuth config: Config) { self.config = config }
-    }
+    class AccessMode: APICategory, ConfluenceAPI {}
+    class Audit: APICategory, ConfluenceAPI {}
+    class Content: APICategory, ConfluenceAPI {}
+    class ContentBody: APICategory, ConfluenceAPI {}
+    class Group: APICategory, ConfluenceAPI {}
+    class LongTask: APICategory, ConfluenceAPI {}
+    class Search: APICategory, ConfluenceAPI {}
+    class Space: APICategory, ConfluenceAPI {}
+    class User: APICategory, ConfluenceAPI {}
 }
 
 public extension Atlassian.Confluence where Infrastructure == Cloud {
-    struct Experimental: APICategory & ConfluenceAPI {
-        public let config: Config
-        public init(withAuth config: Config) { self.config = config }
-    }
-    struct Relation: APICategory & ConfluenceAPI {
-        public let config: Config
-        public init(withAuth config: Config) { self.config = config }
-    }
-    struct Settings: APICategory & ConfluenceAPI {
-        public let config: Config
-        public init(withAuth config: Config) { self.config = config }
-    }
-    struct Template: APICategory & ConfluenceAPI {
-        public let config: Config
-        public init(withAuth config: Config) { self.config = config }
-    }
+    class Experimental: APICategory, ConfluenceAPI {}
+    class Relation: APICategory, ConfluenceAPI {}
+    class Settings: APICategory, ConfluenceAPI {}
+    class Template: APICategory, ConfluenceAPI {}
 }
